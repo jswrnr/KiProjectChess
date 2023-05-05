2ee0956954c076f99ec22ddc2a5e658247dd6109
Jonathan Woerner <jonathan.s.woerner@gmail.com>
2023-05-05 13:48:29 +0200

    basic class setup

---
+++ src/Field.java
@@ -0,0 +1,7 @@
+import pieces.Piece;
+
+public class Field {
+    private int x;
+    private int y;
+    private Piece piece;  
+}
---
+++ src/pieces/Piece.java
@@ -0,0 +1,4 @@
+package pieces;
+public abstract class Piece {
+    private boolean isWhite;
+}
