4ff83969a1936d82ee49b5821c595309533b0932
Jonathan Woerner <jonathan.s.woerner@gmail.com>
2023-05-05 17:51:10 +0200

    basic test for board creation in app

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
+import chess.Board;
+//import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World, hi, hallo!");
-    }
+        //Scanner scanner = new Scanner(System.in);
+        //String input = scanner.nextLine();
+        String input = "8/8/r3k3/8/8/3K4/8/8 b - - 0 31";
+        Board board = new Board(input);
+        System.out.println(board);
+}
 }
