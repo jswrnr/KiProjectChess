33fb763d266a3e32bfb44848e68159bd4dc93c71
Jonathan Woerner <jonathan.s.woerner@gmail.com>
2023-05-05 14:52:23 +0200

    dummy constructor in board class

--- src/Board.java
+++ src/Board.java
@@ -1,3 +1,20 @@
 public class Board {
-    Field[][] board = new Field[8][8];
+    Field[][] board;
+
+    public Board () {
+        this(null);
+    }
+
+    public Board (String fen) {
+
+        if (fen != null) {
+            this.board = createBoardFromFen(fen);
+        } else {
+            this.board = new Field[8][8];
+        }
+    }
+
+    private Field[][] createBoardFromFen(String fen) {
+        return new Field[8][8];
+    }
 }
