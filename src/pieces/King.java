63c2eae8fa6406397ce3335848739ab4f2076b27
Jonathan Woerner <jonathan.s.woerner@gmail.com>
2023-05-13 16:31:06 +0200

    movement for king without castel

--- src/pieces/King.java
+++ src/pieces/King.java
@@ -3,6 +3,7 @@ package pieces;
 import java.util.LinkedList;
 import chess.Move;
 import chess.Board;
+import java.util.Arrays;
 public class King extends Piece {
 
     public King(boolean white) {
@@ -16,6 +17,22 @@ public class King extends Piece {
 
     @Override
     public LinkedList<Move> legalMoves(Board board, int x, int y) {
-        return new LinkedList<Move>();
+        LinkedList<Move> moves = new LinkedList<Move>();
+        //generate all possible moves
+        int[][] possibleMoves = {{x-1, y-1}, {x-1, y}, {x-1, y+1}, {x, y-1}, {x, y+1}, {x+1, y-1}, {x+1, y}, {x+1, y+1}};
+        possibleMoves = Arrays.stream(possibleMoves)
+            //filter moves that are off the board
+            .filter(move -> board.onBoard(move[0], move[1]))
+            //filter moves that are blocked by a piece of the same color
+            .filter(move -> board.getField(move[0], move[1]).getPiece() == null || board.getField(move[0], move[1]).getPiece().isWhite() != this.white)
+            .toArray(int[][]::new);
+        
+        //add all possible moves to the list
+        for(int[] move : possibleMoves){
+            moves.add(new Move(new int[]{x,y}, move, this.white ? 'K' : 'k'));
+        }
+        return moves;
+
+        //todo: castling
     }
 }
